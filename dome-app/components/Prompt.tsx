76ece485daff61a83d6768b654aa79ce36828697
Matias Zapata <matuzapata@gmail.com>
2022-10-16 23:47:20 -0300

    Prompt styles fixes

--- dome-app/components/Prompt.tsx
+++ dome-app/components/Prompt.tsx
@@ -29,25 +29,25 @@ export default function Prompt({
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
       className={
-        "absolute flex flex-1  justify-end left-0 top-0 w-screen h-screen z-50"
+        "absolute flex flex-1 justify-end left-0 top-0 w-screen h-screen z-50"
       }
     >
-      <View className="p-5 m-2 mb-8 bg-white rounded-xl">
-        <Text className="mb-4 text-lg font-bold">{title}</Text>
+      <View className="m-2 mb-8 space-y-4 bg-white rounded-xl">
+        <Text className="p-6 text-lg font-bold text-gray-900">{title}</Text>
         <TextInput
           autoFocus
           placeholder={placeholder}
           defaultValue={defaultValue}
-          className="py-2 border-b-2 border-blue-500"
+          className="mx-6 border-b-2 border-blue-500"
         />
-        <View className="flex flex-row mt-4">
-          <TouchableOpacity className="flex-1 py-2" onPress={() => onCancel()}>
+        <View className="flex flex-row">
+          <TouchableOpacity className="flex-1 py-6" onPress={() => onCancel()}>
             <Text className="text-sm font-medium text-center text-blue-500 uppercase border-r border-gray-200">
               Cancel
             </Text>
           </TouchableOpacity>
           <TouchableOpacity
-            className="flex-1 py-2"
+            className="flex-1 py-6"
             onPress={() => onSubmit("val")}
           >
             <Text className="text-sm font-medium text-center text-blue-500 uppercase">
